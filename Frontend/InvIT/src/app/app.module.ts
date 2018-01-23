ac926ed6b39b882c6cce629dee83e0178e58264c
jzuniga <julio.zuniga@laureate.net>
2018-01-23 10:02:24 -0600

    Se creo la aplicacion en agular ya con bootstrap con dos componentes iniciales footer y el nav bar de momento solo una plantilla inicial funcional se viene el routing y los componentes para crear una hoja de firmas

---
+++ Frontend/InvIT/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    EmployeeTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
